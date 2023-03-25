3de3772c129fb6a9270ddf55c7d8a14c6739e8fd
mayank0591560319 <108619585+mayank0591560319@users.noreply.github.com>
2023-03-25 17:23:01 +0530

    Coding Blocks

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-
-console.log(document);
-console.log(document.getElementById(para));
-console.log(para);
-for(let i=0;i<para.children.length;i++){
-    console.log(para.children[i]);
-}
-const courses=document.getElementById("header");
-console.log(Headers);
-const liItem=document.querySelectorAll('li');
-console.log(liItem);
-const myPara=document.querySelector('#para');
-console.log(myPara);
-const headers=document.getElementById('header');
-console.log(headers);
-for(let course of courses){
-    course.innerText+='-Courses';
-}
+document.getElementById(document);
